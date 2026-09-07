462d1cfe6be9016bf3e3e7470374f7d370106407
febriwinando <35925865+febriwinando@users.noreply.github.com>
2026-09-07 11:23:54 +0700

    pengembangan

---
+++ app/src/main/java/go/pemkott/appsandroidmobiletebingtinggi/camerax/FaceOverlayViewTanpaDeteksi.java
@@ -0,0 +1,58 @@
+package go.pemkott.appsandroidmobiletebingtinggi.camerax;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FaceOverlayViewTanpaDeteksi extends View {
+
+    private Paint paint;
+    private boolean faceInside = false;
+
+    // FRAME NORMALIZED (0–1)
+    private final RectF frameNorm = new RectF(
+            0.05f,  // left
+            0.05f,  // top
+            0.95f,  // right
+            0.7f   // bottom
+    );
+
+    public FaceOverlayViewTanpaDeteksi(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(8f);
+        paint.setAntiAlias(true);
+    }
+
+    public RectF getFrameNormalized() {
+        return frameNorm;
+    }
+
+    public void setFaceInside(boolean inside) {
+        faceInside = inside;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        paint.setColor(faceInside ? Color.GREEN : Color.RED);
+
+        RectF framePx = new RectF(
+                frameNorm.left * getWidth(),
+                frameNorm.top * getHeight(),
+                frameNorm.right * getWidth(),
+                frameNorm.bottom * getHeight()
+        );
+
+        canvas.drawOval(framePx, paint);
+    }
+}
+
